92960868b5a6243f3ae4d2224b86d0e315f07b25
rmit-s3679754-shruti-moudgil <31892877+rmit-s3679754-shruti-moudgil@users.noreply.github.com>
2017-10-08 16:47:29 +0530

    Update World.java

--- src/World.java
+++ src/World.java
@@ -1,8 +1,10 @@
 public class World {
 
+    /* Object myGame is instantiated of class Game */ 
     private static Game myGame = new Game();
 
+    /* Calling runGame() function of Game class in the main() method */
     public static void main(String args[]){
-        myGame.runGame();
+        myGame.runGame();             
     }
 }
