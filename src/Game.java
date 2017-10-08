3fbdc6fc934f7ee4df534d09373b7ff33dad65b8
Vikrant Yadav <vikrant.y.04@gmail.com>
2017-10-09 00:51:25 +1100

    Modifying Game

--- src/Game.java
+++ src/Game.java
@@ -28,11 +28,30 @@ class Game {
 
     private GameItem[][] board = new GameItem[BOARD_SIZE][BOARD_SIZE];
 
+    /**
+     * Generate random board positions from 0 to BOARD_SIZE-1.
+     * @return row,column pair of board position
+     */
     private static int[] generateRandom(){
         int[] coord = {randomGenerator.nextInt(BOARD_SIZE), randomGenerator.nextInt(BOARD_SIZE)};
         return coord;
     }
 
+    /**
+     * Check if row, colunm pair is valid or not based on the BOARD_SIZE
+     * @param coord row, column pair to check if valid
+     * @return coord true or not.
+     */
+    private boolean checkCoordinates(int[] coord){
+        return (coord[0] >= 0 && coord[0] < BOARD_SIZE) && (coord[1] >= 0 &&  coord[1] < BOARD_SIZE);
+    }
+
+    /**
+     * Position objects sent in as parameter at random positions on board
+     * which are not yet filled or empty.
+     * @param elems List of GameItem objects to be inserted in the board
+     * @param count Count of the number of objects to be inserted from the list
+     */
     private void fillElement(GameItem[] elems, int count){
         while(count > 0) {
             int[] coord = generateRandom();
@@ -43,11 +62,21 @@ class Game {
         }
     }
 
+    /**
+     * Function to move back on the board
+     * @param position
+     * @return New position after moving back
+     */
     private int moveBack(int position){
         if(position == 0) return 3;
         else return --position;
     }
 
+    /**
+     * Function to move forward on the board
+     * @param position
+     * @return New position after moving forward
+     */
     private int moveForward(int position){
         if(position == 3) return 0;
         else return ++position;
@@ -60,26 +89,32 @@ class Game {
         //Wrapping wumpus into an array
         GameItem[] wumpusArray = {wumpus};
 
+        // instantiating Gold objects
         for(int goldCount = 0;goldCount < NO_OF_GOLD; goldCount++){
             gold[goldCount] = new Gold();
         }
 
+        // instantiating Pit objects
         for(int pitCount = 0;pitCount < NO_OF_PITS; pitCount++){
             pits[pitCount] = new Pit();
         }
 
+        // Generating a random coordinate to position the player at
         int[] coord = generateRandom();
         int playerCounter = 1;
 
         int wumpusCount = 1;
 
-        /* First we place wumpus in random locations*/
+        /* First we place wumpus in random location */
         fillElement(wumpusArray, wumpusCount);
+
         /* Placing gold in random locations*/
         fillElement(gold, NO_OF_GOLD);
+
         /* Placing pits in random locations*/
         fillElement(pits, NO_OF_PITS);
-        /* Placing clear ground elements in rest of the positions*/
+
+        /* Placing ClearGround objects in rest of the positions */
         for(int i=0;i<BOARD_SIZE;i++){
             for(int j=0;j<BOARD_SIZE;j++){
                 if(board[i][j] == null){
@@ -98,6 +133,9 @@ class Game {
         }
     }
 
+    /**
+     * Display the board
+     */
     private void display(){
         for(int i=0;i<BOARD_SIZE;i++){
             for(int j=0;j<BOARD_SIZE;j++){
@@ -108,6 +146,10 @@ class Game {
         }
     }
 
+    /**
+     * Sensing the elements around the position provided in parameter
+     * @param position Set of coordinates to sense elements around in [row,column] pair
+     */
     private void senseNearBy(int[] position){
         System.out.println("Left: " + board[position[0]][moveBack(position[1])].sense);
         System.out.println("Right: " + board[position[0]][moveForward(position[1])].sense);
@@ -115,6 +157,10 @@ class Game {
         System.out.println("Backward: " + board[moveForward(position[0])][position[1]].sense);
     }
 
+    /**
+     * Showing the menu choices to the player
+     * @return Option value chosen by user
+     */
     private int menu(){
         System.out.println("=====Wumpus====");
         System.out.println("1. Move player left");
@@ -132,8 +178,14 @@ class Game {
         }
     }
 
-    private void updatePosition(){
-        if(board[playerPosition[0]][playerPosition[1]] instanceof Wumpus){
+    /**
+     * Calculate the impact of player's last move and compute score
+     */
+    private void updatePosition() throws Exception{
+        if(!checkCoordinates(playerPosition)){
+            throw new Exception("Invalid player position");
+        }
+        else if(board[playerPosition[0]][playerPosition[1]] instanceof Wumpus){
             System.out.println("You were caught by Wumpus.");
             quit();
         }
@@ -151,36 +203,52 @@ class Game {
         display();
     }
 
+    /**
+     * Display the final score of the player and quit the program
+     */
     private void quit(){
-        System.out.println("Your final score is " + playerScore + ".\nThank you for playing.");
+        System.out.println("Your final score is " +
+                playerScore +
+                ".\nThank you for playing.");
         System.exit(0);
     }
 
+    /**
+     * Run the game by setting and displaying the board and taking user choices and
+     * updating positions
+     */
     public void runGame(){
         setBoard();
         display();
         while(true){
             int input = menu();
-            switch(input){
-                case 1:
-                    playerPosition[1] = moveBack(playerPosition[1]);
-                    updatePosition();
-                    break;
-                case 2:
-                    playerPosition[1] = moveForward(playerPosition[1]);
-                    updatePosition();
-                    break;
-                case 3:
-                    playerPosition[0] = moveBack(playerPosition[0]);
-                    updatePosition();
-                    break;
-                case 4:
-                    playerPosition[0] = moveForward(playerPosition[0]);
-                    updatePosition();
-                    break;
-                case 5:
-                    quit();
-                    break;
+            try{
+                switch(input){
+                    case 1:
+                        playerPosition[1] = moveBack(playerPosition[1]);
+                        updatePosition();
+                        break;
+                    case 2:
+                        playerPosition[1] = moveForward(playerPosition[1]);
+                        updatePosition();
+                        break;
+                    case 3:
+                        playerPosition[0] = moveBack(playerPosition[0]);
+                        updatePosition();
+                        break;
+                    case 4:
+                        playerPosition[0] = moveForward(playerPosition[0]);
+                        updatePosition();
+                        break;
+                    case 5:
+                        quit();
+                        break;
+                    default:
+                        throw new Exception("Invalid user option encountered");
+                }
+            }catch (Exception e){
+                System.out.println("Exception encountered " + e.toString());
+                continue;
             }
         }
     }
