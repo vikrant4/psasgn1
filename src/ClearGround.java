a96642d1aeec79db3a8cbacf03bdfcce6993ac53
rmit-s3679754-shruti-moudgil <31892877+rmit-s3679754-shruti-moudgil@users.noreply.github.com>
2017-10-08 16:50:07 +0530

    Update ClearGround.java

--- src/ClearGround.java
+++ src/ClearGround.java
@@ -1,5 +1,6 @@
 class ClearGround extends GameItem{
 
+    /* Function ClearGround() is called which inherits GameItem class */
     ClearGround(){
         super('.', "nothing can be sensed");
     }
